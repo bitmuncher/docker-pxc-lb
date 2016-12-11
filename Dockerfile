ac03ae3e34350d767094219701e3cc6c8480183a
Frank Fuhrmann <frank.fuhrmann@mailbox.org>
2016-12-11 01:30:06 +0100

    initial version of image informations

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM debian:latest
+MAINTAINER Frank Fuhrmann <frank.fuhrmann@mailbox.org>
+
+RUN apt-get -y update 
+RUN apt-get -y upgrade
+
+RUN apt-get -y install haproxy python
+
+COPY conf/haproxy.cfg /etc/haproxy/haproxy.cfg
+COPY start.py /start.py
+RUN chmod a+rx /start.py
+
+EXPOSE 3306
+EXPOSE 8181
+
+CMD sh -c '/start.py && /usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid'
---
+++ start.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from subprocess import call
+
+configfile = '/etc/haproxy/haproxy.cfg'
+template = 'server name ip:3306 check port 9200 inter 12000 rise 3 fall 3'
+
+serverlist = os.environ['SERVERS']
+
+if not serverlist:
+    print "No servers defined. Exit!"
+    sys.exit(100)
+
+servers = serverlist.split(',')
+addservers = ''
+configdata = ''
+
+i = 1
+for server in servers:
+    name = 'pxc' + str(i)
+    ip = server
+    print 'Name: ' + name + ' IP: ' + ip
+    mystr = template;
+    mystr = mystr.replace('name', name)
+    mystr = mystr.replace('ip', ip)
+    print "mystr: " + mystr
+    addservers = addservers + '    ' + mystr + "\n"
+    print 'addservers: ' + addservers
+    i = i + 1
+    
+with open(configfile, 'r') as myfile:
+    configdata = myfile.read().replace('{servers}', addservers)
+    
+f = open(configfile, 'w')
+f.write(configdata)
